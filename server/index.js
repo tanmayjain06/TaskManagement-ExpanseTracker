29f6e369f74d8c2d462ffa5fa662120d96800fc4
CodeWaveWithAsante <132752907+CodeWaveWithAsante@users.noreply.github.com>
2024-03-19 12:29:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["https://mern-task-mnger.netlify.app", "http://localhost:3000", "http://localhost:3001"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
