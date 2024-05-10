5081f4e0b996de6ebcf052fc76a102924c8a4814
CodeWaveWithAsante <132752907+CodeWaveWithAsante@users.noreply.github.com>
2024-05-10 12:10:43 +0530

    Update index.js

--- server/utils/index.js
+++ server/utils/index.js
@@ -8,7 +8,7 @@ const createJWT = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks
+    sameSite: "none", // Prevent CSRF attacks
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
 };
